359f7b44cf9bb01be53ffe751b49df85ec11124a
Miraslau Rabikau <m.rabikau@vironit.co.uk>
2024-01-08 12:27:57 +0300

    feature: pagination has been added

--- src/components/feed/index.tsx
+++ src/components/feed/index.tsx
@@ -2,12 +2,17 @@
 import React, { useEffect, useState } from 'react';
 import PostList from '@/components/post-list';
 import { IPost } from '@/models/post.interface';
+import PaginationControl from '@/components/pagination-control';
+
+const itemsPerPage = 5;
 
 const Feed = () => {
   const [searchText, setSearchText] = useState<string>('');
   const [posts, setPosts] = useState<IPost[]>([]);
   const [searchedResults, setSearchedResults] = useState<IPost[]>([]);
   const [searchTimeOut, setSearchTimeOut] = useState<any>(null);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [currentPosts, setCurrentPosts] = useState<IPost[]>([]);
 
   const fetchPosts = async () => {
     const resp = await fetch('/api/post');
@@ -19,6 +24,17 @@ const Feed = () => {
   useEffect(() => {
     fetchPosts();
   }, []);
+
+  useEffect(() => {
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const newCurrentItems = posts.slice(indexOfFirstItem, indexOfLastItem);
+    setCurrentPosts(newCurrentItems);
+  }, [posts, itemsPerPage, currentPage]);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
 
   const filterPosts = (text: string) => {
     const regex = new RegExp(text, 'i');
@@ -63,7 +79,14 @@ const Feed = () => {
       {searchText ? (
         <PostList posts={searchedResults} handleTagClick={handleTagClick} />
       ) : (
-        <PostList posts={posts} handleTagClick={handleTagClick} />
+        <PostList posts={currentPosts} handleTagClick={handleTagClick} />
+      )}
+      {(searchedResults.length > 0 || posts.length > 0) && (
+        <PaginationControl
+          itemsPerPage={itemsPerPage}
+          data={searchText ? searchedResults : posts}
+          handlePageChange={handlePageChange}
+        />
       )}
     </section>
   );
